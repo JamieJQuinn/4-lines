2afa78653f3978c6aa5d8d25c58cf6c6ce8855a6
Jamie Quinn <j.quinn.1@research.gla.ac.uk>
2018-05-16 12:48:05 +0100

    Using perlin noise to set the vertices of 4-gons for a continuous random feel

--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
-var SIZE=512
+var SIZE=1024
 var ONE=1.0/SIZE
-var FORM_MARGIN=0.05*SIZE
+var FORM_MARGIN=0.03*SIZE
 var MARGIN=FORM_MARGIN
 var N_FORMS=10 // 10x10 forms
 var FORM_SIZE=(SIZE - 2*MARGIN - (N_FORMS-1)*FORM_MARGIN)/N_FORMS
+var NOISE_PARAM = 10;
 
 function setup() {
   canvas = createCanvas(SIZE, SIZE);
@@ -28,9 +29,27 @@ function draw() {
   }
 }
 
+function rand(x, y) {
+  return noise(x, y) - 0.5;
+}
+
+function random_vector(x, y) {
+  return createVector(rand(x, y), rand(x, y));
+}
+
 function draw_form(x, y) {
-  line(x-FORM_SIZE/2, y-FORM_SIZE/2, x+FORM_SIZE/2, y-FORM_SIZE/2);
-  line(x+FORM_SIZE/2, y-FORM_SIZE/2, x+FORM_SIZE/2, y+FORM_SIZE/2);
-  line(x+FORM_SIZE/2, y+FORM_SIZE/2, x-FORM_SIZE/2, y+FORM_SIZE/2);
-  line(x-FORM_SIZE/2, y+FORM_SIZE/2, x-FORM_SIZE/2, y-FORM_SIZE/2);
+  p1 = createVector(x-FORM_SIZE/2, y-FORM_SIZE/2);
+  p2 = createVector(x+FORM_SIZE/2, y-FORM_SIZE/2);
+  p3 = createVector(x+FORM_SIZE/2, y+FORM_SIZE/2);
+  p4 = createVector(x-FORM_SIZE/2, y+FORM_SIZE/2);
+
+  p1.add(random_vector(NOISE_PARAM*p1.x/SIZE, NOISE_PARAM*p1.y/SIZE).mult(FORM_SIZE));
+  p2.add(random_vector(NOISE_PARAM*p2.x/SIZE, NOISE_PARAM*p2.y/SIZE).mult(FORM_SIZE));
+  p3.add(random_vector(NOISE_PARAM*p3.x/SIZE, NOISE_PARAM*p3.y/SIZE).mult(FORM_SIZE));
+  p4.add(random_vector(NOISE_PARAM*p4.x/SIZE, NOISE_PARAM*p4.y/SIZE).mult(FORM_SIZE));
+
+  line(p1.x, p1.y, p2.x, p2.y);
+  line(p2.x, p2.y, p3.x, p3.y);
+  line(p3.x, p3.y, p4.x, p4.y);
+  line(p4.x, p4.y, p1.x, p1.y);
 }
